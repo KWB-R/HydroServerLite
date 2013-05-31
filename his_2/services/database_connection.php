b63a60ca4d8d614b5f630d9c6fc27dd4363bfdc1
jirikadlec2 <jirikadlec2@gmail.com>
2013-05-31 20:44:40 +0300

    fix db connection file

--- his_2/services/database_connection.php
+++ his_2/services/database_connection.php
@@ -1,5 +1,15 @@
 <?php
-require_once '../client/main_config.php';
+# The database configuration is loaded either from
+# the main_config.php file in the client directory.
+# for example (www.example.com/his_2/client/main_config.php).
+# If main_config.php is not found then the file:
+# services_config.php is used.
+if (file_exists('../client/main_config.php')) {
+    require_once '../client/main_config.php';
+} else {
+    require_once 'services_config.php';
+}
+
 $connection = mysql_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD) 
 or
 die("<p>Error connecting to database: ".mysql_error()."</p>");
