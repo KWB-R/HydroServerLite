e5ebb76836a4731274539c7782ce49cc338115cb
jirikadlec2 <jirikadlec2@gmail.com>
2013-06-26 17:03:21 +0300

    set example values of db name and password

--- codehydro/application/config/services.php
+++ codehydro/application/config/services.php
@@ -1,4 +1,4 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 //Service code Settings
-$config['auth_token'] = 'h1dr0d47a';
-$config['service_code'] = 'KALA';
+$config['auth_token'] = '';
+$config['service_code'] = 'MYSERVICE';
