c258eafbb3f4846e1709f10fd99d60cc5e7e88e4
jirikadlec2 <jirikadlec2@gmail.com>
2013-07-03 23:16:00 +0300

    services - placeholder for service code

--- services/application/config/services.php
+++ services/application/config/services.php
@@ -1,4 +1,4 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 //Service code Settings
-$config['auth_token'] = '';
-$config['service_code'] = 'OGIMET-PERU';
+$config['auth_token'] = ''; /* set this value if you want to password-protect the service */
+$config['service_code'] = 'YOUR_SERVICE_CODE';
