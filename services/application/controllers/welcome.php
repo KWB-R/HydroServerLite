e44ac13cbea1cc7fa0bf039067a546185632a279
lucian bursuc <lucianbursuc@gmail.com>
2013-11-19 11:44:32 +0200

    test commit title - my first commit on hydroserverlite

--- services/application/controllers/welcome.php
+++ services/application/controllers/welcome.php
@@ -19,7 +19,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('index');
+		$data['title'] = "Test commit title";
+		$this->load->view('index', $data);
 	}
 }
 
