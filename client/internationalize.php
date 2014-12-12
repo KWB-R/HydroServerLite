f57c5a077fd22787e2853b3a2722849414735fbd
sarva <sarvateja@gmail.com>
2014-12-11 17:02:40 -0700

    New and improved

--- client/internationalize.php
+++ client/internationalize.php
@@ -56,14 +56,7 @@
 	$page_text = $urlAddon."languages/" . $lang_code . "/" . $lang_file;
 	$common_text = $urlAddon."languages/" . $lang_code . "/_common_text.php";
 	
-	//Check If files exist before opening
-
-if (file_exists($page_text))
-	{
-	include($page_text);}
-if (file_exists($common_text)){
-	include_once($common_text);
-}
+		
 	
 	}
 	
