745e4463c46070a4228992428dac477d243e8f24
sarva <sarvateja@gmail.com>
2014-12-22 21:43:51 -0700

    recommit

--- application/controllers/sites.php
+++ application/controllers/sites.php
@@ -26,9 +26,113 @@ class Sites extends MY_Controller {
 	{		
 		//List of CSS to pass to this view
 		$data=$this->StyleData;
-		$this->load->view('site/add',$data);
+		$this->load->view('add_site',$data);
 	}
 	
+	public function edit()
+	{		
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('edit_site',$data);
+	}
+	public function addsource()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('add_source',$data);
+		
+	}
+	public function changesource()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('change_source',$data);
+		
+	}
+	public function editvariable()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('edit_var',$data);
+		
+	}
+	public function addvariable()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('add_variable',$data);
+		
+	}
+	public function addmethod()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('add_method',$data);
+		
+	}
+	public function changemethod()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('change_method',$data);
+		
+	}
+	public function adduser()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('adduser',$data);
+		
+	}
+	public function changeyourpassword()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('change_yourpassword',$data);
+		
+	}
+	public function changepassword()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('changepassword',$data);
+		
+	}
+	public function changeauthority()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('changeauthority',$data);
+		
+	}
+	public function removeuser()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('removeuser',$data);
+		
+	}
+	public function adddatavalue()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('add_data_value',$data);
+		
+	}
+	public function addmultiplevalues()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('add_multiple_values',$data);
+		
+	}
+	public function importdatafile()
+	{
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('import_data_file',$data);
+		
+	}
 	public function index()
 	{
 		
