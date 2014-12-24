d78cbc390a8c18edddf114a26c3f1edcf6ecf0e7
sarva <sarvateja@gmail.com>
2014-12-24 14:52:54 -0700

    source controller

---
+++ application/controllers/source.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+|--------------------------------------------------------------------------
+| Sites Controller
+|--------------------------------------------------------------------------
+|
+| 
+*/
+class Source extends MY_Controller {
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('sources','',TRUE);
+	}
+	
+	public function addsource()
+	{		
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('sources/add_source',$data);
+	}
+	
+	public function editsource()
+	{		
+		//List of CSS to pass to this view
+		$data=$this->StyleData;
+		$this->load->view('sources/change_source',$data);
+	}
+	
+}
