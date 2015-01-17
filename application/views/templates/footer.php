20228b2524a2640a6b5bf289c367e99d07b1c242
Jeremy Fowler <jkfowler12.18@gmail.com>
2015-01-17 15:16:37 -0700

    Added a space in the html for looks

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
 <i><?php echo getTxt('CopyRight');?> &copy; <?php echo date('Y'); ?>. 
 <a href='http://hydroserverlite.codeplex.com/' target='_blank' class='reversed'><?php echo getTxt('SystemName');?></a>. 
-<?php echo getTxt('AllRightsReserved');?><a href='http://hydroserverlite.codeplex.com/team/view' target='_blank' class='reversed'><?php echo getTxt('MeetDevelopers');?></a></i>
+<?php echo getTxt('AllRightsReserved');?><a href='http://hydroserverlite.codeplex.com/team/view' target='_blank' class='reversed'> &nbsp; <?php echo getTxt('MeetDevelopers');?></a></i>
 </div>
