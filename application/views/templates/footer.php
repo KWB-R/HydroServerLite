369b7eef5d502b73d81d2a3786ad07b28f46bbf9
Jeremy Fowler <jkfowler12.18@gmail.com>
2015-03-18 16:38:05 -0600

    changes to the footer page

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,7 +2,7 @@
 <i><?php echo getTxt('CopyRight');?> &copy; <?php echo date('Y'); ?>. 
 <a href='http://hydroserverlite.codeplex.com/' target='_blank' class='reversed'><?php echo getTxt('SystemName');?></a>
 <?php echo getTxt('version');?>
-<?php echo $this->config->item('HSLversion');?>. 
+<?php echo $this->config->item('HSLversion');?>&nbsp
 <?php echo getTxt('AllRightsReserved');?><a href='http://hydroserverlite.codeplex.com/team/view' target='_blank' class='reversed'> <?php echo getTxt('MeetDevelopers');?></a>
 
 </i>
